a8c228599f1c493db50c07e8e7ccb716f550ab82
Robert Fischer <artiefischer@gmail.com>
2019-01-07 16:38:30 -0700

    react shaping and forming

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import {Navbar, NavItem, Button} from 'react-materialize'
+import {Button} from 'react-materialize'
 import SignUp from './components/SignUp'
+import HeaderCMP from './components/HeaderCMP'
 
 class App extends Component {
   render() {
     return <div className="App">
-        <header className="App-header">
-          <Navbar className="cyan lighten-4" left>
-            <ul className="nav navbar-nav navbar-right black">
-              <li>
-                <img id="logo" src="https://www.gmri.org/sites/default/files/logo_0.png" height="60" alt="" />
-                <img src="https://static1.squarespace.com/static/5a75f43a692ebeeb1159413d/t/5adf368f2b6a28995d5d1539/1524577943240/Seal.Navy.png" height="60" width="60" alt="" />
-              </li>
-            </ul>
-            <NavItem className="cyan" onClick={() => console.log("test click")}>
-              F.A.S. React Frontend
-            </NavItem>
-          </Navbar>
-        </header>
+        <HeaderCMP />
         <SignUp />
         <footer>
           <Button className="footer-btn right grey">About GMRI</Button>
---
+++ src/components/AboutGMRI.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AboutGMRI = () => {
+  return <div>
+      GMRI is cool! <span aria-label="Close" role="img">
+        🌊
+      </span>
+    </div>;
+}
+
+export default AboutGMRI;
